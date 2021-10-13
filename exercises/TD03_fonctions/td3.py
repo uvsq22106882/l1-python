72c767fc172b64d8d9c656b3dc486dda3ecbbb95
Prénom NOM <prenom.nom@ens.uvsq.fr>
2021-10-13 15:56:24 +0200

    test

---
+++ exercises/TD03_fonctions/td3.py
@@ -0,0 +1,25 @@
+temps=(3,23,1,34)
+def tempsEnSeconde (a):
+     seconde = temps[0]*86400+temps[1]*3600+temps[2]*60+temps[3] 
+     return seconde 
+print(tempsEnSeconde(temps))     
+print(type(temps))
+
+def secondeEnTemps (t):
+     jour=0
+     heure=0
+     minute=0
+     while t>=86400 :
+       t=t-86400
+       jour=jour+1
+     while t>=3600 :
+        t=t-3600
+        heure=heure+1 
+     while t>=60 :
+         t=t-60 
+         minute=minute+1
+     temps(jour,heure,minute,t) 
+     return temps 
+temps=secondeEnTemps(100000)
+print(temps)
+print(type(temps))  
